fbc6ea2dd8acb220825c24084a8fa0850d41c014
James Ridley <jamesridley3@gmail.com>
2016-08-22 15:54:14 -0500

    we now have cookies to remember age gate

--- app/index/controller.js
+++ app/index/controller.js
@@ -6,14 +6,25 @@ export default Ember.Controller.extend({
   isLoggedIn: false,
   userIsOfAge: false,
 
+  cookies: Ember.inject.service(),
+
   queryParams: {
     isRegisterModalVisible: 'register',
     isLoginModalVisible: 'login',
   },
 
+  init() {
+    this._super(...arguments);
+    // check if there is cookie value for user_is_of_age???
+    if (this.get('cookies').read('user_is_of_age')) {
+      this.set('userIsOfAge', true);
+    }
+  },
+
   actions: {
     allowUserToEnter: function() {
-        this.toggleProperty('userIsOfAge')
+      this.toggleProperty('userIsOfAge');
+      this.get('cookies').write('user_is_of_age', 'true');
     },
 
     blockUserFromSite: function() {
