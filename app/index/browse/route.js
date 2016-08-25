f3a5749f0f58e15f9903b39e19927cf3be231a13
James Ridley <jamesridley3@gmail.com>
2016-08-25 15:00:43 -0500

    wip: scrape data coming in and being displayed

--- app/index/browse/route.js
+++ app/index/browse/route.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+    return this.store.query('drink', {
+      page: {
+        number: params.page,
+        size: params.size,
+      },
+    });
+  },
+
+  queryParams: {
+    page: {
+      refreshModel: true,
+    },
+    size: {
+      refreshModel: true,
+    },
+  },
 });
