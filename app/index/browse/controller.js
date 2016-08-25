66028465aec384f7458a87d10369cc2fd242eed9
James Ridley <jamesridley3@gmail.com>
2016-08-25 16:13:46 -0500

    wip: search-bar component

--- app/index/browse/controller.js
+++ app/index/browse/controller.js
@@ -4,4 +4,13 @@ export default Ember.Controller.extend({
   queryParams: ['page', 'size'],
   page: 1,
   size: 200,
+  actions: {
+    filterByDrink(param) {
+      if (param !== '') {
+        return this.get('store').query('drink', { name: param });
+      } else {
+        return this.get('store').findAll('drink');
+      }
+    },
+  },
 });
