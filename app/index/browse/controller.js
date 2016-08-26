29b1c68d391c9463e2257b14a5824e968ced8e57
James Ridley <jamesridley3@gmail.com>
2016-08-26 14:00:39 -0500

    search bar works on browse page

--- app/index/browse/controller.js
+++ app/index/browse/controller.js
@@ -3,14 +3,13 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   queryParams: ['page', 'size'],
   page: 1,
-  size: 200,
+  size: 50,
   actions: {
     filterByDrink(param) {
       if (param !== '') {
         return this.get('store').query('drink', {
           name: param,
           page: {
-            number: this.page,
             size: this.size,
           },
         });
