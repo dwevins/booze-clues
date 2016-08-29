fa0bade24d02001bb6365b6b40ecda8591fac063
James Ridley <jamesridley3@gmail.com>
2016-08-29 12:08:15 -0500

    wip: cabinet search is searching for ingredients

--- app/models/ingredient.js
+++ app/models/ingredient.js
@@ -2,5 +2,5 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  recipeIngreidents: DS.hasMany('recipe-ingreident')
+  recipeIngreidents: DS.hasMany('recipe-ingreident'),
 });
