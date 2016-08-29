d2b4932260c2a362c9ac641f69bc7faae1cafcb9
Danny Evins <dcastkid@gmail.com>
2016-08-29 12:19:03 -0500

    browse searching works now

--- app/components/list-filter/component.js
+++ app/components/list-filter/component.js
@@ -4,20 +4,23 @@ export default Ember.Component.extend({
   classNames: ['list-filter'],
   value: '',
 
-  // Runs everytime a new version of this component is created
-  init() {
+  // Runs everytime the attributes for this component change
+  didReceiveAttrs() {
     // Does the setup or the normal version of "init" so we don't have to rewrite that...
     this._super(...arguments);
 
     // Gets data when the component first is created with an empty search
-    this.get('filter')('').then((drinks) => this.set('drinks', drinks));
+    this.get('filter')(this.query, this.get('page')).then((drinks) => this.set('drinks', drinks));
+
+    this.set('value', this.query);
   },
 
   actions: {
     handleFilterEntry() {
       let filterInputValue = this.get('value');
       let filterAction = this.get('filter');
-      filterAction(filterInputValue).then((filterResults) => this.set('drinks', filterResults));
+      let page = this.get('page');
+      filterAction(filterInputValue, page).then((filterResults) => this.set('drinks', filterResults));
     },
   },
 
--- app/index/browse/controller.js
+++ app/index/browse/controller.js
@@ -1,22 +1,37 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  queryParams: ['page', 'size'],
+  queryParams: ['page', 'size', 'query'],
   page: 1,
   size: 10,
+  query: '',
+
+  resetQuery(query) {
+    this.setProperties({
+      page: 1,
+      query,
+    });
+  },
+
   actions: {
-    filterByDrink(param) {
-      if (param !== '') {
+    filterByDrink(query, page) {
+      page = page || this.page;
+
+      if (query !== this.query) {
+        this.resetQuery(query);
+      }
+      if (query !== '') {
         return this.get('store').query('drink', {
-          name: param,
+          name: query,
           page: {
+            number: page,
             size: this.size,
           },
         });
       } else {
         return this.get('store').query('drink', {
           page: {
-            number: this.page,
+            number: page,
             size: this.size,
           },
         });
@@ -24,8 +39,7 @@ export default Ember.Controller.extend({
     },
 
     getNextPage() {
-      this.incrementProperty("page");
-
+      this.incrementProperty('page');
     },
   },
 });
