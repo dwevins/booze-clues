516059107a9c117757de79ff8878af973d4d90cc
Danny Evins <dcastkid@gmail.com>
2016-08-30 17:45:03 -0500

    can submit favorite

--- app/index/recipe/controller.js
+++ app/index/recipe/controller.js
@@ -1,23 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-session: Ember.inject.service(),
-  actions: {
+  session: Ember.inject.service(),
 
+  actions: {
     saveFave() {
-      console.log(this.get('session').store);
       const attrs = {
-        user: this.get('session'),
-        drink: this.get('model.id'),
-      }
-      // console.log(attrs);
+        drink: this.get('model'),
+      };
       const favorite = this.store.createRecord('favorite', attrs);
-      // favorite.save().then(() => {
-      //   this.get('flashMessages').success('Bill Murray likes that drink too.');
-      // })
-      // .catch(() => {
-      //   this.get('flashMessages').warning('duh, already know that.');
-      // })
+      favorite.save().then(() => {
+        this.get('flashMessages').success('Bill Murray likes that drink too.');
+      })
+      .catch(() => {
+        this.get('flashMessages').warning('duh, already know that.');
+      })
     },
   },
 });
--- app/models/favorite.js
+++ app/models/favorite.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  user: DS.attr('number'),
-  drink: DS.attr('number'),
+  user: DS.belongsTo('user'),
+  drink: DS.belongsTo('drink'),
 });
