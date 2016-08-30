3f8c52ecbf2667ecd2eca4ed4acce19bda6906c2
Danny Evins <dcastkid@gmail.com>
2016-08-30 15:17:02 -0500

    working on getting current user id

--- app/index/recipe/controller.js
+++ app/index/recipe/controller.js
@@ -5,9 +5,10 @@ export default Ember.Controller.extend({
   actions: {
     saveFave() {
       const attrs = {
-        user_id: this.get('secure.user_id'),
+        user_id: this.get('session'),
         drink_id: this.get('model.id'),
       }
+      console.log(attrs);
       const favorite = this.store.createRecord('favorite', attrs);
       favorite.save().then(() => {
         this.get('flashMessages').success('Bill Murray likes that drink too.');
