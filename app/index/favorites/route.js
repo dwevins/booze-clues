a2229a86979944732ac10d7499cd2e6b57dec662
Danny Evins <dcastkid@gmail.com>
2016-08-31 17:09:39 -0500

    shows list of user's favorites

--- app/index/favorites/route.js
+++ app/index/favorites/route.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
   queryParams: {
     page: {
       refreshModel: true,
@@ -9,5 +8,9 @@ export default Ember.Route.extend({
     size: {
       refreshModel: true,
     },
+  },
+
+  model() {
+    return this.store.findAll('favorite');
   },
 });
