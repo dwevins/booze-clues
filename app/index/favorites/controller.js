4299b8b41f834dd2a637021bf8135d29f852c8dc
Danny Evins <dcastkid@gmail.com>
2016-09-01 10:51:19 -0500

    some cleanup

--- app/index/favorites/controller.js
+++ app/index/favorites/controller.js
@@ -1,13 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  queryParams: ['page', 'size'],
-  page: 1,
-  size: 10,
 
-  actions: {
-    getNextPage() {
-      this.incrementProperty('page');
-    },
-  },
 });
--- app/index/favorites/route.js
+++ app/index/favorites/route.js
@@ -1,15 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  queryParams: {
-    page: {
-      refreshModel: true,
-    },
-    size: {
-      refreshModel: true,
-    },
-  },
-
   model() {
     return this.store.findAll('favorite');
   },
